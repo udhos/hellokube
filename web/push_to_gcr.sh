e250e1f4e577ed8a93344e700a7fbcbd2c36f521
udhos <everton.marques@gmail.com>
2018-07-02 16:44:38 -0300

    Push to gcr with tag.

--- web/push_to_gcr.sh
+++ web/push_to_gcr.sh
@@ -9,22 +9,27 @@ die() {
         exit 1
 }
 
+[ -n "$PORT" ] || die missing env var PORT=$PORT
+
+[ "$#" -eq 1 ] || die usage: $0 tag
+tag=$1
+
 mkdir eraseme
 
 cat >eraseme/Dockerfile <<__EOF__ || die could not create Dockerfile
 FROM udhos/web:latest
 ENV PORT 8888
-CMD ["/gowebhello -addr 8888"]
+ENTRYPOINT ["/gowebhello", "-addr", ":$PORT", "-banner", "tag=$tag"]
 __EOF__
 
 pushd eraseme || die could not enter dir
 
 project_id="$(gcloud config get-value project -q)"
 
-docker build -t gcr.io/$project_id/web:v1 .
+docker build -t gcr.io/$project_id/web:$tag .
 
-docker push gcr.io/$project_id/web:v1
+docker push gcr.io/$project_id/web:$tag
 
 popd
 
-echo test with: docker run --rm -p 8888:8888 gcr.io/$project_id/web:v1
+echo test with: docker run --rm -p $PORT:$PORT gcr.io/$project_id/web:$tag
